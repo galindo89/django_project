0e9bd4aab15d43d7b13a715cc702672dffe2dd78
Pablo <pablo@livedooh.com>
2024-11-28 00:10:32 +0100

    updating debug mode

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pbwny9z2mgzdv!iqupec)pchb2s++q*8#)-l1805-y4oqdo^5('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com'
